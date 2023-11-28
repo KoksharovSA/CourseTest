fa11dea03a4d1c47df3dda0cea55e990e9395f30
SKoksharov <konsist-koksharovsa@yandex.ru>
2023-11-28 14:49:34 +0700

    Done hw4.

---
+++ src/test/java/org/example/hw4/BookServiceTest.java
@@ -0,0 +1,32 @@
+package org.example.hw4;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BookServiceTest {
+    static BookRepository mockBookRepository = mock(BookRepository.class);
+    static Book book = new Book("1","aa","bb");
+    static List<Book> books= Arrays.asList(book);
+    BookService mockBookService = new BookService(mockBookRepository);
+    @BeforeAll
+    static void beforeTesting(){
+        when(mockBookRepository.findById("1")).thenReturn(book);
+        when(mockBookRepository.findAll()).thenReturn(books);
+    }
+    @Test
+    void findBookById() {
+        assertThat(mockBookService.findBookById("1")).isEqualTo(book);
+    }
+
+    @Test
+    void findAllBooks() {
+        assertThat(mockBookService.findAllBooks()).isEqualTo(books);
+    }
+}
